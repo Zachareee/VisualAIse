aea1cee829a7e10a5e374820e70c50bf2a49c6b7
Zachareee <zacharytanhx@yahoo.com>
2024-05-08 12:19:00 +0800

    Changed auth flow to protect all routes

--- index.mjs
+++ index.mjs
@@ -1,9 +1,11 @@
 import express from "express"
 import { Miro } from "@mirohq/miro-api"
-import Storage from "./store.mjs"
 import cookieParser from "cookie-parser"
 import { resolve } from "path"
 import { renderFile } from "ejs"
+
+// custom utils
+import Storage from "./store.mjs"
 
 // Variables
 const PORT = process.env.port || 3000
@@ -23,9 +25,11 @@ app.get("/", async (req, res) => {
     const { session } = req.cookies
     const { user } = req.query
 
+    // make sure user is authed or trying to auth
     if (!(session || user))
         return res.status(401).send("Missing user param")
 
+    // retrieve token and store with provided user name
     if (!await miro.isAuthorized(session))
         return res.render(getStaticFile("unauth.html"), { link: miro.getAuthUrl(user) })
 
@@ -37,6 +41,15 @@ app.get("/auth", async (req, res) => {
     miro.handleAuthorizationCodeRequest(user, req)
     res.cookie("session", user)
 
+    return res.redirect("/")
+})
+
+// mounted after "/" to avoid infinite redirects
+// after "/auth" to avoid intercepting auth code
+app.use(async (req, res, next) => {
+    const { session } = req.cookies
+
+    if (await miro.isAuthorized(session)) return next()
     return res.redirect("/")
 })
 
