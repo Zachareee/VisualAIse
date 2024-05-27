16b997bdd605dde7ac2fbc4bf812e161db458c6b
Zachareee <zacharytanhx@yahoo.com>
2024-05-27 23:54:39 +0800

    Something works? Idk? Code adapted from https://github.com/AndreyGermanov/yolov8_onnx_nodejs

---
+++ src/clustering.mjs
@@ -0,0 +1,35 @@
+// Code adapted from https://github.com/AndreyGermanov/yolov8_onnx_nodejs/blob/main/object_detector.js
+import ort from "onnxruntime-node"
+import sharp from "sharp"
+
+// tensor only allows 640(?)
+const imageRes = 640
+
+async function readImage(path) {
+    const image = sharp(path)
+    const metadata = await image.metadata()
+    const { width, height } = metadata
+
+    const pixels = await image.removeAlpha()
+        .resize(imageRes, imageRes)
+        .raw()
+        .toBuffer()
+
+    const output = []
+    for (var i = 0; i < 3; i++) {
+        for (var j = i; j < pixels.length; j += 3) output.push(pixels[j])
+    }
+
+    return { width, height, output }
+}
+
+async function runModel({ output: input }) {
+    const model = await ort.InferenceSession.create("yolov8m.onnx")
+    const output = new ort.Tensor(Float32Array.from(input), [1, 3, imageRes, imageRes])
+
+    const data = await model.run({ images: output })
+    console.log(data)
+    return data["output0"].data
+}
+
+readImage("screenshots/ss.png").then(output => runModel(output))
